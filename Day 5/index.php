ae54e84a91198865049cf7c38ad156b8bbc93347
Xander Tamis <xtamis2000@gmail.com>
2022-12-06 11:54:18 +0100

    Day 5 completed

--- Day 5/index.php
+++ Day 5/index.php
@@ -1,9 +1,33 @@
 <?php
+    $stacks = [
+        1 => ['W', 'D', 'G', 'B', 'H', 'R', 'V'],
+        2 => ['J','N','G','C','R','F'],
+        3 => ['L', 'S', 'P', 'H', 'D', 'N', 'J'],
+        4 => ['J', 'D', 'S', 'V'],
+        5 => ['S', 'H', 'D', 'R', 'Q', 'W', 'N', 'V'],
+        6 => ['P', 'G', 'H', 'C', 'M'],
+        7 => ['F', 'J', 'B', 'G', 'L', 'Z', 'H', 'C'],
+        8 => ['S', 'J', 'R'],
+        9 => ['L', 'G', 'S', 'R', 'B', 'N', 'V', 'M']
+    ];
+
     $handle = fopen("input.txt", "r");
     if ($handle) {
         while (($line = fgets($handle)) !== false) {
-            $values = explode(",", $line);
+            if(str_starts_with($line, 'move')) {
+                preg_match_all('!\d+!', $line, $numbers);
+                $result = $numbers[0];
+                $crates = array_splice($stacks[$result[1]], -1 * $result[0], $result[0]);
+
+                // Uncomment for answer of part 1.
+                //$crates = array_reverse($crates);
+                array_push($stacks[$result[2]], ...$crates);
+            }
         }
         fclose($handle);
+    }
+
+    foreach ($stacks as $stack) {
+        echo end($stack);
     }
 ?>
